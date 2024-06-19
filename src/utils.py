742a21062f7b2e03990e2390df212735d81d264c
Alina Baranova <alina_nemo@mail.ru>
2024-06-19 17:09:22 +0300

    Delete useless file

--- src/utils.py
+++
@@ -1,7 +0,0 @@
-class UserInput:
-    def __init__(self, user_input):
-        self.user_input = user_input
-
-    def search_vacation(self):
-        if self.user_input == 1:
-            print('1. Поиск вакансии')
